c09735a077df62c974f5ee85c0262f3d0981ebab
Jignesh Gohel <jignesh.gohel@botreeconsulting.com>
2013-03-22 01:10:22 +0530

    WLJK Rails application skeleton containing Rails application's conventional folders and files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wljk::Application.config.secret_token = '5714ca5e945d09a26bb94730b2466759c533a0c1ceaf4ef5e7c2d97c3d245d9918befd25a50aa0649726c70e575dcb30fe139128eaa4c494269418cdb0b02a47'
